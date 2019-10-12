1ecdc133e0793bab6161eeb1abc169760a84637b
Trey Westrich <treyw05@gmail.com>
2019-10-12 11:56:25 -0500

    added scopes to movies

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -19,17 +19,11 @@ class Movie < ApplicationRecord
 
   validates :rating, inclusion: { in: RATINGS }
 
-  def self.released
-    where("released_on <= ?", Time.now).order("released_on desc")
-  end
-
-  def self.hits
-    where('total_gross >= 300000000').order(total_gross: :desc)
-  end
-
-  def self.flops
-    where('total_gross < 50000000').order(total_gross: :asc)
-  end
+  scope :released, -> { where("released_on <= ?", Time.now).order("released_on desc") }
+  scope :unreleased, -> { where("released_on > ?", Time.now).order("released_on asc") }
+  scope :recent, ->(max=3) { released.limit(max) }
+  scope :hits, -> {where('total_gross >= 300000000').order(total_gross: :desc)}
+  scope :flops, -> {where('total_gross < 50000000').order(total_gross: :asc)}
 
   def self.recently_added
     order('created_at desc').limit(3)
